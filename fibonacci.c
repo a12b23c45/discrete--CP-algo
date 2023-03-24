c147c370a0cdbe43cced121d1d44b887334bb6f8
a12b23c45 <128162211+a12b23c45@users.noreply.github.com>
2023-03-24 21:38:04 +0530

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,25 @@
+//fibonacci series
+//complexity=O(logn)
+
+#include <stdio.h>
+
+int fast_doub(unsigned long int n) {
+    
+    if (n<=2){
+        return n?1:0;
+    }
+    else{
+        unsigned long int k=(n/2);
+        unsigned long int a=fast_doub(k);
+        unsigned long int b=fast_doub(k+1);
+        return (n%2)?(a*a+(b*b)):(a*(2*b-a));
+    }
+}
+
+int main()
+{
+    unsigned long int n;
+    scanf("%lu",&n);
+    printf("%lu",fast_doub(n));
+    return 0;
+}
